24d5239042b75b1bd601e7e58d3c0f21e64c77d7
TJPBray <t.bray@ucl.ac.uk>
2025-04-22 09:55:50 +0100

    Modification of figure settings for training curves

--- Visualisation/trainingCurves.m
+++ Visualisation/trainingCurves.m
@@ -5,19 +5,20 @@ trainingRMSE = net.info1.TrainingRMSE;
 validationRMSE = net.info1.ValidationRMSE;
 
 % Define scale factor for max
-scaling = [0.1 0.45];
+scaling = [0.2 0.3];
 
 
 %% 
 figure
 subplot(1,2,1)
-plot(smooth(trainingRMSE,100))
+plot(smooth(trainingRMSE,5000),'LineWidth',2,'MarkerSize',6,'Marker','.')
 ylim(scaling*max(trainingRMSE))
+ylabel('Training RMSE','FontSize',16)
 
 subplot(1,2,2)
-plot(validationRMSE(validationRMSE>0),'k')
+plot(smooth(validationRMSE(validationRMSE>0),100),'k', 'LineWidth',2,'MarkerSize',6,'Marker','.')
 ylim(scaling*max(trainingRMSE))
-
+ylabel('Validation RMSE','FontSize',16)
 end
 
 
