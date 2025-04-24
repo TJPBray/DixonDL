302b9944360ba3a17a2e05f81bafac34a83db10c
TJPBray <t.bray@ucl.ac.uk>
2025-04-24 14:46:30 +0100

    Fully vectorised implementation of likelihood calculation

--- Network/sseVecCalc.m
+++ Network/sseVecCalc.m
@@ -9,34 +9,61 @@ function [sseVec,likVec] = sseVecCalc (echotimes, tesla, predictionVec, S, sigma
 % Output:
 % likVec is an output of likelihood values based on the above
 
-% 1. Get n
+%% 1. Get n
 n=size(predictionVec,1);
 
 % Prefill arrays
 p = zeros(4,1);
 
 
-%2. Loop over n values
-parfor k=1:n
+%% 2. Loop implementation over n values
+
+% parfor k=1:n
+% 
+%     %2.1 First specify components of p
+%     f=predictionVec(k,1)*predictionVec(k,3);
+%     w=(1-predictionVec(k,1))*predictionVec(k,3);
+%     v=predictionVec(k,2);
+%     fB=0;
+% 
+%     p = [f; w; v; fB];
+%     p = double(p);
+% 
+%     Smeasured=S(k,:);
+% 
+%     %2.2 Calculate likelihood (use Gaussian initially)
+%     [gaussianLikVec(k),sseVec(k)] = R2Obj(p,echotimes,tesla,Smeasured,sigma); %Use sigma of 0 - note that likelilihood values with be NaN
+% 
+%     %2.2 Calculate likelihood (Rician)
+%     [ricianLikVec(k)] = R2RicianObj(p,echotimes,tesla,Smeasured,sigma); %Use sigma of 0 - note that likelilihood values with be NaN
+% 
+% end
+
+%% 2. Vectorised implementation
 
     %2.1 First specify components of p
-    f=predictionVec(k,1)*predictionVec(k,3);
-    w=(1-predictionVec(k,1))*predictionVec(k,3);
-    v=predictionVec(k,2);
-    fB=0;
+    f=predictionVec(:,1).*predictionVec(:,3);
+    w=(1-predictionVec(:,1)).*predictionVec(:,3);
+    v=predictionVec(:,2);
+    fB=zeros(size(v));
 
-    p = [f; w; v; fB];
+    p = [f w v fB]';
     p = double(p);
-
-    Smeasured=S(k,:);
+  
+    Smeasured=S;
 
     %2.2 Calculate likelihood (use Gaussian initially)
-    [gaussianLikVec(k),sseVec(k)] = R2Obj(p,echotimes,tesla,Smeasured,sigma); %Use sigma of 0 - note that likelilihood values with be NaN
+    [gaussianLikVec,sseVec] = R2Obj(p,echotimes,tesla,Smeasured,sigma); %Use sigma of 0 - note that likelilihood values with be NaN
+
+    gaussianLikVec = gaussianLikVec';
+    sseVec = sseVec';
 
     %2.2 Calculate likelihood (Rician)
-    [ricianLikVec(k)] = R2RicianObj(p,echotimes,tesla,Smeasured,sigma); %Use sigma of 0 - note that likelilihood values with be NaN
+    [ricianLikVec] = R2RicianObj(p,echotimes,tesla,Smeasured,sigma); %Use sigma of 0 - note that likelilihood values with be NaN
 
-end
+    ricianLikVec = ricianLikVec';
+
+%%    
 
 likVec = ricianLikVec;
 
